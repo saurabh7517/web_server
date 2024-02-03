423c7436f6bec52ffef456e8ab3f09982e3596b6
Saurabh Kumar <saurabh7517@gmail.com>
2024-02-03 08:22:15 +0000

    creating a file server

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
+	var fileServer http.Handler = http.FileServer(http.Dir("./static"))
+	var logger *log.Logger = log.New(os.Stdout, time.Now().String()+" :: INFO :: ", 1)
 
-	fmt.Println("hello world")
+	http.Handle("/", fileServer)
+	logger.Printf("Starting Http Server")
+	var err error = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		logger.Fatal("Server startup failed !!")
+	}
+	logger.Printf("Stopping Http Server")
 
 }
